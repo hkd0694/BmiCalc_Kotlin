9f37b04e046cb6ef1f68e3e6d82c007b39852698
hkd0694 <gksrudehd123@naver.com>
2019-10-29 15:28:56 +0900

    Add ResultActivity

--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -3,12 +3,10 @@ package com.example.bmicalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
+        setContentView(R.layout.activity_result)
     }
 }
