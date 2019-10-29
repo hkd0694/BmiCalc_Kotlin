aa6b8a7c0c1626250478218a1c908bc5e13e9983
hkd0694 <gksrudehd123@naver.com>
2019-10-29 16:11:34 +0900

    fix : Intent, Toast코드 추가 및 정리

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,13 +2,31 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*;
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //코틀린으로 만든 프로젝트에는 기본적으로 'kotlin-android-extensions' 플러그인이 설정이 되어 있기 때문에
+        //레이아웃에 배치한 뷰 ID가 자동으로 임포트되어 코틀린 코드에서 바로 사용할 수 있다.
+        //java 에선 findViewId를 해줘야 사용 가능!!
+        resultButton.setOnClickListener {
+            //Java에선 new 연산자를 통해 Intnet를 만들어주지만     (ResultActivity.class)
+            //Kotlin에서는 new 연산자를 사용 안해도 된다.          (ResultActivity::class.java)
+            /*val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("weight",weightEditText.text.toStrong())
+            intent.putExtra("height",heightEditText.text.toStrong())
+            startActivity(intent)
+            */
+            //하지만 우리가 더 간편하게 Anko Library를 사용하면 코드가 더 짧게 표현할 수 있다.
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
+        }
 
-        
     }
 }
--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -2,11 +2,52 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.toast
 
 class ResultActivity : AppCompatActivity() {
+
+    /*
+    * Up Navigation
+    * 화면 상단 왼쪽에 뒤로가기 버튼을 만들기 위해 AndroidManifest.xml 파일에
+    * android:name=".ResultActivity" 인 <activity>로 가 <android:parentActivityName=".MainActivity"/> 를 추가
+    * 추가해주면 ResultActivity 화면 상단에 뒤로가기 버튼이 추가되어있다. (초간단)...
+    * */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        //Java에서는 getIntent()를 통해 intent 정보를 받아와 getStringExtra() 함수를 부르지만
+        //Kotlin에서는 getter() setter()가 자동으로 호출되기 때문에 intent.getStringExtra() 함수를 바로 호출할 수 있다.
+        val weight = intent.getStringExtra("weight").toInt()
+        val height = intent.getStringExtra("height").toInt()
+
+        //Bmi 계산하는 방법
+        val bmi = weight / Math.pow(height / 100.0,2.0)
+
+        //when은 Java에서 사용하는 switch문을 코틀린에서는 when으로 사용 한다.
+        //switch문에서는 나머지를 표현할 때 default를 사용하지만
+        //when 문에서는 else 를 사용한다.
+        when {
+            bmi >= 35 -> resultTextView.text = "고도 비만"
+            bmi >= 30 -> resultTextView.text = "2단계 비만"
+            bmi >= 25 -> resultTextView.text = "1단계 비만"
+            bmi >= 23 -> resultTextView.text = "과체중"
+            bmi >= 18.5 -> resultTextView.text = "정상"
+            else -> resultTextView.text = "저체중"
+        }
+
+        when {
+            bmi >= 23 -> imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            bmi >=18.5 -> imageView.setImageResource(R.drawable.ic_sentiment_satisfied_black_24dp)
+            else -> imageView.setImageResource(R.drawable.ic_sentiment_dissatisfied_black_24dp)
+        }
+
+        //Toast 메시지를 사용한 것
+        //원래 표시할때는 Toast.makeText(this,"$bmi",Toast.Length_SHORT).show()를 사용하지만
+        //Anko 라이블리를 사용하면 아래 코드처럼 정말 간단하게 Toast 메시지를 추가할 수 있다.
+        toast("$bmi")
+
     }
 }
